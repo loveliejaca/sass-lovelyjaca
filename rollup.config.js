4cfccad9351a37dcaffe59cecf069483c7069a06
Lovely Jaca <jlablab@gmail.com>
2022-04-07 20:38:23 +0800

    update format type

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ export default [
     input: 'src/js/scripts.js',
     output: {
       file: 'dist/src/js/scripts.js',
-      format: 'iief',  //error in format IDK why
+      format: 'iife',
       sourcemap: devMode ? 'inline' : false,
       plugins: [terser()]
     },
